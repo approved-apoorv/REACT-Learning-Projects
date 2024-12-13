445054aebfad893f2c04efc79e0cceb97851e1b9
Apoorv Baghel <apoorvsingh075@gmail.com>
2024-12-13 13:22:02 +0530

    First commit

---
+++ 01counter/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [count, setCount] = useState(0)
+  
+  const addValue = ()=>{
+    setCount(count+1)
+  }
+
+  const subtractValue = ()=>{
+    if(count>0){
+      setCount(count-1)
+    }
+  }
+
+  return (
+    <>
+      <h1>The count is {count}</h1>
+      <button
+      onClick={addValue}
+      >Add value</button>
+      <button
+      onClick={subtractValue}
+      >Subtract value</button>
+    </>
+  )
+}
+
+export default App
