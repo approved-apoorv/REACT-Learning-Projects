21d75edeed6b1c00647dcfa3cf20a21482057729
Apoorv Baghel <apoorvsingh075@gmail.com>
2024-12-22 23:50:05 +0530

    Creating the base

--- 04passwordGenerator/src/App.jsx
+++ 04passwordGenerator/src/App.jsx
@@ -1,10 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useCallback } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8)
+  const [numEnable, setNumEnable] = useState(false);
+  const [charEnable, setCharEnable] = useState(false);
+  const [password, setPassword] = useState("")
+  const passwordGenerator = useCallback(() => {
+    let pass =""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if(numEnable) str += "0123456789"
+    if(charEnable) str += ":!@#$%^&*()-_=+{};:/?.>"
+
+    for (let i = 1; i < array.length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      
+    }
+
+  }, [length, numEnable, charEnable, setPassword])
 
   return (
     <>
