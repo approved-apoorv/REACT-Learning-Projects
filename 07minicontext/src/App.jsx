376aa53505223b1a8ed36c6a1b857cdcd3d1fb9a
Apoorv Baghel <apoorvsingh075@gmail.com>
2025-06-06 13:25:03 +0530

    Context api basics

--- 07minicontext/src/App.jsx
+++ 07minicontext/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
 
 function App() {
 
   return (
-    <>
-    <div>React hai bhai</div>
-    </>
+    <UserContextProvider>
+    <Login />
+    <Profile />
+    </UserContextProvider>
   )
 }
 
