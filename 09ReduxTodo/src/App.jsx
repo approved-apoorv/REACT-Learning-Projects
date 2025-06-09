c2fb9932628a8549a22582fbeb21e28641660d6d
Apoorv Baghel <apoorvsingh075@gmail.com>
2025-06-09 17:02:06 +0530

    Redux toolkit Project

---
+++ 09ReduxTodo/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+import { Provider } from 'react-redux'
+import { store } from './app/store'
+
+function App() { 
+
+  return (
+    <Provider store={store}>
+      <h1>Hello Redux !!</h1>
+      <AddTodo />
+      <Todos />
+    </Provider>
+  )
+}
+
+export default App
